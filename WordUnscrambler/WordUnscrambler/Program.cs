6a37ac50581ddedc02db7f5c660d62f3f9251cb2
JackGudgeon <50985837+JackGudgeon@users.noreply.github.com>
2019-12-17 08:28:06 +0000

    Added project files.

---
+++ WordUnscrambler/WordUnscrambler/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WordUnscrambler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ListExample();
+            ArrayExample();
+        }
+
+        private static void ListExample()
+        {
+            List<int> myList = new List<int>(); //Dynamically adjusted - no initital capacity.
+
+            Console.WriteLine(myList.Count());
+
+            myList.Add(10);
+            myList.Add(5);
+
+            Console.WriteLine(myList.Count());
+            Console.ReadLine();
+        }
+
+        private static void ArrayExample()
+        {
+            int[] myArray = new int[10]; //Needs an initial capacity.
+        }
+    }
+}
